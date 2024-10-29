5b8a0999c7b1d773b913b642d41ff7eac3d7f9d2
WoongyuChoi <schna@kakao.com>
2024-10-29 10:01:18 +0900

    feature : QueryClientProvider 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from ".";
 import "./App.css";
 import ChartProvider from "./provider/ChartProvider";
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <ChartProvider>
-      <BrowserRouter>
-        <SpeedInsights />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </ChartProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChartProvider>
+        <BrowserRouter>
+          <SpeedInsights />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ChartProvider>
+    </QueryClientProvider>
   );
 }
 
