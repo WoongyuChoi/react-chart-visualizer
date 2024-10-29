44fce817236280a27729d1219c66a2de7afbae68
WoongyuChoi <schna@kakao.com>
2024-10-29 11:03:06 +0900

    feature : BarChartNegative 동적 Data 처리

--- src/chart/BarChartNegative.tsx
+++ src/chart/BarChartNegative.tsx
@@ -1,8 +1,27 @@
 import { Bar } from "react-chartjs-2";
-import { data, options } from "../data/BarChartNegativeConstant";
+import { data as initialData, options } from "../data/BarChartNegativeConstant";
+import useDynamicData from "../hook/useDynamicData";
 
 const BarChartNegative = ({ chartRef }: { chartRef: React.RefObject<any> }) => {
-  return <Bar ref={chartRef} data={data} options={options} />;
+  const { data, containerRef } = useDynamicData(
+    initialData,
+    (prevData) => ({
+      ...prevData,
+      datasets: prevData.datasets.map((dataset: { data: any[] }) => ({
+        ...dataset,
+        data: dataset.data.map(() =>
+          Math.floor(Math.random() * (-1750000000 + 1))
+        ),
+      })),
+    }),
+    "BarChartNegative"
+  );
+
+  return (
+    <div ref={containerRef}>
+      <Bar ref={chartRef} data={data} options={options} />
+    </div>
+  );
 };
 
 export default BarChartNegative;
