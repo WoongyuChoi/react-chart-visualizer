36059a64b7dd45454ed976ce44a99441962093df
WoongyuChoi <schna@kakao.com>
2024-10-29 11:19:33 +0900

    feature : WaterfallChart 동적 Data 처리

--- src/chart/WaterfallChart.tsx
+++ src/chart/WaterfallChart.tsx
@@ -1,16 +1,76 @@
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import React from "react";
 import { Bar } from "react-chartjs-2";
-import { data, options } from "../data/WaterfallChartConstant";
+import { data as initialData, options } from "../data/WaterfallChartConstant";
+import useDynamicData from "../hook/useDynamicData";
 
 const WaterfallChart = ({ chartRef }: { chartRef: React.RefObject<any> }) => {
+  const { data, containerRef } = useDynamicData(
+    initialData,
+    (prevData) => {
+      let cumulativeValue = 0;
+      let totalValue = 0;
+
+      const newData = prevData.datasets.map((dataset: { data: any[] }) => ({
+        ...dataset,
+        data: dataset.data.map((_, index) => {
+          if (index === 0) {
+            // 첫 번째 데이터는 [0, ?] 형식
+            const change =
+              Math.floor(Math.random() * (50000 - 1000 + 1)) + 1000;
+            const newValue = cumulativeValue + change;
+            cumulativeValue = newValue;
+            totalValue += change;
+            return [0, newValue];
+          } else if (index === dataset.data.length - 1) {
+            // 마지막 데이터는 [0, 총합계] 형식
+            return [0, totalValue];
+          } else {
+            const change =
+              Math.floor(Math.random() * (50000 - 1000 + 1)) + 1000;
+            const isIncrease = Math.random() > 0.5;
+
+            let newValue;
+            if (isIncrease) {
+              newValue = cumulativeValue + change;
+            } else {
+              newValue = cumulativeValue - change;
+              if (newValue < 0) {
+                newValue = 0; // 0 이하로 내려가지 않도록 제한
+              }
+            }
+
+            // totalValue가 음수가 되지 않도록 조정
+            if (newValue > cumulativeValue) {
+              totalValue += change;
+            } else {
+              totalValue = Math.max(0, totalValue - change);
+            }
+
+            const result = [cumulativeValue, newValue];
+            cumulativeValue = newValue;
+            return result;
+          }
+        }),
+      }));
+
+      return {
+        ...prevData,
+        datasets: newData,
+      };
+    },
+    "WaterfallChart"
+  );
+
   return (
-    <Bar
-      ref={chartRef}
-      data={data}
-      options={options}
-      plugins={[ChartDataLabels]}
-    />
+    <div ref={containerRef}>
+      <Bar
+        ref={chartRef}
+        data={data}
+        options={options}
+        plugins={[ChartDataLabels]}
+      />
+    </div>
   );
 };
 
